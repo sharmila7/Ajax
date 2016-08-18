cfe481468fb416220f395c54a563381e3483dad4
sharmila7 <sharmibtech.sk@gmail.com>
2016-08-18 10:08:13 +0530

    Create script.js

---
+++ javascripts/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded",
+	function (event) {
+
+		//unobstrusive event binding
+		document.querySelector("button").addEventListener("click",
+			function () {
+				var self = this;
+				var name = "";
+
+				$ajaxUtils.sendGetRequest("data/name.txt",
+					function (request) {
+						console.log("hi");
+						self.name = request.responseText;
+						cosole.log(self.name);
+					});
+				document.querySelector("#content").innerHTML =
+				 "Hello"+self.name+"!";
+			})
+	}
+	);
